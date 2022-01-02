86960e3c734cb0f4266205be3bbb2462258a0d29
Gabriela Neves <42540317+GabrielaNeves@users.noreply.github.com>
2022-01-01 23:49:43 -0300

    Implementação de método construtor, encapsulamento, readonly e variável estática

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,14 @@
 export class Cliente{
     nome;
-    cpf;
+    _cpf;
+
+    //metodo get com o construtor para que o cpf n seja mais alterado
+    get cpf(){
+        return this._cpf;
+    }
+
+    constructor(nome, cpf){
+        this.nome = nome;
+        this._cpf = cpf;
+    }
 }
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,11 +1,34 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-    cliente;
+    _cliente;
 
-
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
+
+    //instanciando para que o cliente só aceite valores da classe
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        };
+    };
+
+    //acessor para que eu consiga pegar somente o cliente
+    get cliente(){
+        return this._cliente;
+    }
     
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
